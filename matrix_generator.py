0e1850a91a22df4119b77530d2a9a4c049e1d539
Poruch <139491864+Poruch@users.noreply.github.com>
2025-11-22 19:06:53 +0500

    create test generator

--- matrix_generator.py
+++ matrix_generator.py
@@ -1,11 +1,25 @@
 import random
 from collections import namedtuple
-
+from main import calculate_determinant
 Case = namedtuple("Case", ["matrix", "det"])
 MAX_RANDOM_VALUE = 10
 MIN_RANDOM_VALUE = 1
 
-
+def get_det(mat):
+    sum = 0
+    if(len(mat) > 2):
+        for i in range(len(mat)):        
+            newMat = [[0 for k in range(len(mat) - 1)]for j in range(len(mat) - 1)]
+            for j in range(1,len(mat)):
+                count = 0
+                for k in range(len(mat)):
+                    if(k != i):
+                        newMat[j-1][count] = mat[j][k]
+                        count += 1
+            sum += (-1)**(i)* mat[0][i] * get_det(newMat)
+    else:
+        sum += mat[1][1]*mat[0][0] - mat[0][1] * mat[1][0]
+    return sum
 def generate_matrix_and_det(order) -> Case:
     """Генерирует случайную квадратную целочисленную матрицу с заранее
     известным значением определителя.
@@ -15,11 +29,15 @@ def generate_matrix_and_det(order) -> Case:
     меньше 1
     :return: именованный кортеж Case с полями matrix, det
     """
-    pass
+    matrix = []
+    for i in range(order):
+        matrix.append(list([int(random.random() * 100 )for i in range(order)]))
+    print(matrix)
+    return Case(matrix,calculate_determinant(matrix))
 
 
 def main():
-    n = 10
+    n = 5
     print(f"Генерация матрицы порядка {n}")
     result = generate_matrix_and_det(n)
     print("\nОпределитель сгенерированной матрицы равен", result.det)
