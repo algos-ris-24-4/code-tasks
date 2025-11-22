409b2fb0a39f04dabe19b56378ff9b77d5686cb5
Poruch <139491864+Poruch@users.noreply.github.com>
2025-11-22 18:40:04 +0500

    complete task-2-v1

--- main.py
+++ main.py
@@ -6,11 +6,53 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
 
+    #валидация
+    validate(matrix)
+    #рекурсия
+    return calculate_determinant_rec(matrix)
+
+def calculate_determinant_rec(matrix):
+    """
+    Рекурсивная часть вычисления определителя
+    """
+    #база рекурсии
+    if len(matrix) == 1:
+        return matrix[0][0]
+    det = 0
+    for i in range(len(matrix[0])):
+        reduce = get_reduce_mat(matrix,i)
+        det += matrix[0][i]*(-1)**i*calculate_determinant_rec(reduce)    
+    return det
+
+
+def validate(matrix):
+    """
+    Проверка матрицы на возможность вычисления определителя
+    """
+    l = len(matrix)
+    for i in matrix:
+        if(len(i) != l):
+            raise Exception("Not square")
+        for j in i:
+            if(type(j).__name__ != "int" ):
+                raise Exception("Not int")
+
+def get_reduce_mat(mat, i):
+    """
+    Вохвращает новую матрицу с пропущенной 1 строкой и i+1 столбцом
+    """
+    newMat = [[0 for k in range(len(mat) - 1)]for j in range(len(mat) - 1)]
+    for l in range(1,len(mat)):
+        count = 0
+        for k in range(len(mat)):
+            if(k != i):
+                newMat[l-1][count] = mat[l][k]
+                count += 1
+    return newMat
 
 def main():
-    matrix = [[1, 2], [3, 4]]
+    matrix = [[1, 2], [3,4]]
     print("Матрица")
     for row in matrix:
         print(row)
