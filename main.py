45858f07238c8295ca67f257316b349884919395
Нисимова Даниела <dnisimova@inbox.ru>
2025-11-24 11:55:51 +0500

    slaaay задание 2

--- main.py
+++ main.py
@@ -6,7 +6,40 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
+    validate(matrix)
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+    
+    det = 0
+    for idx, item in enumerate(matrix):
+       reduced matrix = get_reduced_matrix(matrix, 0, idx)
+       det += item * (-1)**idx * calculate_determinant(reduced matrix)
+    return det
+
+def get_reduced_matrix(matrix, row idx, col idx):
     pass
+
+
+
+def validate(matrix):
+    if not isinstance(matrix, list):
+        raise Exception("Ошибка: Входные данные не являются списком.")
+
+    if len(matrix) == 0:
+        raise Exception("Ошибка: Матрица пуста.")
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise Exception("Ошибка: Строки матрицы не являются списками.")
+        if len(row) != len(matrix):
+            raise Exception("Ошибка: Матрица не является квадратной.")
+
+    for row in matrix:
+        for element in row:
+            if not isinstance(element, int):
+                raise Exception("Ошибка: Матрица содержит нецелочисленные элементы.")
+
 
 
 def main():
