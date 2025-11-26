240268c433620aeb8b80ced9671782649cc818ac
Кирилл Семёнов <topchikkirya@gmail.com>
2025-11-26 20:34:13 +0500

    Almost done

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 def calculate_determinant(matrix: list[list[int]]) -> int:
-    """Вычисляет определитель целочисленной квадратной матрицы
+    n = len(matrix)
+    det = 0
+    
+    if matrix is None or n == 0:
+        raise Exception("Матрица не может быть None или пустой")
+    
+    if any(any(not isinstance(x, int) for x in row) for row in matrix):
+        raise Exception("Все элементы матрицы должны быть целыми числами")
+    
+    for row in matrix:
+        if len(row) != n:
+            raise Exception("Матрица не является квадратной")
+        
+    if n == 1:
+        return matrix[0][0]
+    
+    elif n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    
+    else:
+        for j in range(n):
+        
+            minor = [row[:j] + row[j+1:] for row in matrix[1:]]
+        
+            sign = 1 if j % 2 == 0 else -1
+        
+            det += sign * matrix[0][j] * calculate_determinant(minor)
 
-    :param matrix: целочисленная квадратная матрица
-    :raise Exception: если значение параметра не является целочисленной
-    квадратной матрицей
-    :return: значение определителя
-    """
-    pass
+    return det
+
 
 
 def main():
