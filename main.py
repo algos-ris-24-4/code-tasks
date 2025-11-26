52a66b5a8124c762a4c297e6506e069cd68d511d
INPershin <ilya.pershin217@gmail.com>
2025-11-26 13:03:28 +0500

    task 2 v1 is ready by kotakaenjoyers

--- main.py
+++ main.py
@@ -6,7 +6,37 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    validate(matrix)
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    det = 0
+    for idx, item in enumerate(matrix[0]):
+        reduced_matrix = get_reduced_matrix(matrix, idx)
+        det += item * (-1)**idx * calculate_determinant(reduced_matrix)
+    return det
+
+def get_reduced_matrix(matrix, column):
+    result = []
+    for i in range(1, len(matrix)):
+        current_row = matrix[i]
+        result.append(current_row[:column] + current_row[column + 1:])
+    return result
+
+def validate(matrix: list[list[int]]):
+    if not matrix or not isinstance(matrix,list):
+        raise Exception("Матрица должна быть непустым списком")
+    length = len(matrix)
+    for i in range(len(matrix)):
+        row = matrix[i]
+        if not isinstance(row,list):
+            raise Exception("Каждый ряд матрицы должен быть списком чисел")
+        if len(row) != len(matrix):
+            raise Exception("Матрица должна быть квадратной")
+        for j in row:
+            if not isinstance(j,int):
+                raise Exception("Каждый элемент матрицы должен быть целым числом")
 
 
 def main():
