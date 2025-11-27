b4dc52a8e1a78e946febe8d4d267100dbbb3b29d
DonkeyChoke <127410409+DonkeyChoke@users.noreply.github.com>
2025-11-27 11:52:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def fibonacci_iter(n: int) -> int:
         return 1
     
     arr = [0] * n
-    arr[0], arr[1] = 1
-
+    arr[0] = 1
+    arr[1] = 1
     for i in range(2, n):
         arr[i] = arr[i - 1] + arr[i - 2]
 
