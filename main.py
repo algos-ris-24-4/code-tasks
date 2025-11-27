d477cf70f0aa15906d9ed53db5b474d055f67513
DonkeyChoke <127410409+DonkeyChoke@users.noreply.github.com>
2025-11-27 11:51:10 +0500

    апдейт

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ def fibonacci_rec(n: int) -> int:
         return fibonacci_rec(n-2)+fibonacci_rec(n-1)
 
 def fibonacci_iter(n: int) -> int:
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    
+    arr = [0] * n
+    arr[0], arr[1] = 1
+
+    for i in range(2, n):
+        arr[i] = arr[i - 1] + arr[i - 2]
+
+    return arr[n - 1]
+
+def fibonacci(n: int) -> int:
     if n <= 0:
         return 0
     elif n == 1:
@@ -15,9 +29,6 @@ def fibonacci_iter(n: int) -> int:
     for i in range(2, n + 1):
         a, b = b, a + b
     return b
-
-def fibonacci(n: int) -> int:
-    return fibonacci_iter(n)
 
 
 def main():
@@ -29,7 +40,7 @@ def main():
     print(fibonacci_iter(n))
 
     print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
-    print(fibonacci_iter(n))
+    print(fibonacci(n))
 
 
 if __name__ == "__main__":
