6a730416e3a6967bf2670fdb20171447c40a71fc
INPershin <ilya.pershin217@gmail.com>
2026-01-11 22:47:09 +0500

    test fix

--- tests/problems/knapsack_problem/test_dynamic.py
+++ tests/problems/knapsack_problem/test_dynamic.py
@@ -3,8 +3,8 @@ import unittest
 from problems.knapsack_problem.dynamic_solver import DynamicSolver
 from problems.knapsack_problem.errors.error_message_enum import ErrorMessageEnum
 from tests.problems.knapsack_problem.test_abstract import TestAbstractSolver
-
-
+from problems.knapsack_problem.errors.error_message_template import ErrorMessageTemplateEnum
+from problems.knapsack_problem.errors.constants import WEIGHTS
 class TestDynamicSolver(unittest.TestCase, TestAbstractSolver):
     """Набор тестов для проверки решения задачи о рюкзаке методом
     динамического программирования."""
@@ -14,10 +14,10 @@ class TestDynamicSolver(unittest.TestCase, TestAbstractSolver):
     def test_float_in_weights(self):
         """Проверяет выброс исключения при передаче не целочисленного значения
         в списке весов."""
-        with self.assertRaises(ValueError) as error:
+        with self.assertRaises(TypeError) as error:
             self.solver([1, 1.1], [1, 1], 1)
         self.assertEqual(
-            ErrorMessageEnum.FLOAT_WEIGHT,
+            ErrorMessageTemplateEnum.NOT_INT.format(WEIGHTS),
             str(error.exception),
         )
 
