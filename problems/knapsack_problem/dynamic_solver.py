a80bb95405076f9ed3bb54bb9a2157286869ea5c
INPershin <ilya.pershin217@gmail.com>
2026-01-11 21:38:48 +0500

    task 6 v4 is ready by kotakaenjoyers

--- problems/knapsack_problem/dynamic_solver.py
+++ problems/knapsack_problem/dynamic_solver.py
@@ -12,7 +12,38 @@ class DynamicSolver(KnapsackAbstractSolver):
         :return: максимально возможная общая стоимость и список индексов выбранных предметов
         :rtype: KnapsackSolution
         """
-        pass
+        i_cnt = self.item_cnt
+        w_limit = self.weight_limit
+        weights = self.weights
+        costs = self.costs
+
+        table = [[0] * (w_limit+1) for _ in range(i_cnt+1)]
+
+        for i in range(1, i_cnt + 1):
+            wi = weights[i-1]
+            ci = costs[i-1]
+            for j in range(w_limit + 1):
+                table[i][j] = table[i-1][j]
+                if wi <= j:
+                    cost = table[i-1][j-wi] + ci
+                    if cost > table[i][j]:
+                        table[i][j] = cost
+
+        idxs = [] 
+        w = w_limit
+
+        for i in range(i_cnt, 0, -1):
+            if table[i][w] != table[i-1][w]:
+                idxs.append(i-1)
+                w -= weights[i-1]
+
+        idxs.reverse()
+
+        result = table[i_cnt][w_limit]
+
+        return KnapsackSolution(result, idxs)
+
+        
 
 
 if __name__ == "__main__":
