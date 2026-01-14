82dd8cc970a4d20d29d3fb8d27e0103cbcc6bcff
Лопатин Иван <sanivan19032006@gmail.com>
2026-01-14 20:40:06 +0500

    task complete

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 STR_LENGTH_ERROR_MSG = "Длина строки должна быть целым положительным числом"
-"""Сообщение об ошибке при некорректном значении параметра Длина строки"""
 
 NOT_INT_VALUE_TEMPL = "Параметр {0} Не является целым числом"
-"""Шаблон сообщения об ошибке при нечисловом значении параметра"""
 
 NEGATIVE_VALUE_TEMPL = "Параметр {0} отрицательный"
-"""Шаблон сообщения об ошибке при отрицательном значении параметра"""
 
 N_LESS_THAN_K_ERROR_MSG = "Параметр n меньше чем k"
-"""Сообщение об ошибке при значении параметра n меньше чем k"""
 
 
 def generate_strings(length: int) -> list[str]:
-    """Возвращает строки заданной длины, состоящие из 0 и 1, где никакие
-    два нуля не стоят рядом.
-
-    :param length: Длина строки.
-    :raise ValueError: Если длина строки не является целым положительным
-    числом.
-    :return: Список строк.
-    """
-    pass
+        
+        if not isinstance(length,int) or length <= 0:
+            raise ValueError(STR_LENGTH_ERROR_MSG)
+        def zero(length):
+            if length == 1:
+                return ["0"]
+            return ["0" + stroka for stroka in one(length-1)]
+        
+        def one(length):
+            if length == 1:
+                return ["1"]
+            return ["1" + stroka for stroka in zero(length-1) + one(length-1)]
+        
+        return zero(length) + one(length)
 
 
 def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
-    """Вычисляет биномиальный коэффициент из n по k.
-    :param n: Количество элементов в множестве, из которого производится выбор.
-    :param k: Количество элементов, которые нужно выбрать.
-    :param use_rec: Использовать итеративную или рекурсивную реализацию функции.
-    :raise ValueError: Если параметры не являются целыми неотрицательными
-    числами или значение параметра n меньше чем k.
-    :return: Значение биномиального коэффициента.
-    """
-    pass
-
+    
+    if not isinstance(n,int):
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("n"))
+    if not isinstance(k,int):
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("k"))
+    if n < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("n"))
+    if k < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("k"))
+    if n < k:
+        raise ValueError(N_LESS_THAN_K_ERROR_MSG)
+    
+    if use_rec:
+        return binomial_rec(n, k)
+    else:
+        return binomial_iter(n, k)
+def binomial_rec(n, k):
+    if k == n or k == 0:
+        return 1
+    return binomial_rec(n - 1, k) + binomial_rec(n - 1, k - 1)
+def binomial_iter(n, k):
+    if k == n or k == 0:
+        return 1
+    dp = [[0] * (k+1) for i in range(n+1)]
+    for i in range(n + 1):
+        dp[i][0] = 1
+        for j in range(1, min(i, k) + 1):
+            if j == i:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+    return dp[n][k]
 
 def main():
     n = 2
