e631b95261fe1f8c5dda824fa35cbb8b722b03f4
Poruch <139491864+Poruch@users.noreply.github.com>
2026-02-05 15:37:26 +0500

    fix structure

--- matching/perfect_matching.py
+++ matching/perfect_matching.py
@@ -23,60 +23,62 @@ def get_perfect_matching(bipartite_graph: BipartiteGraph) -> BipartiteGraphMatch
     order = bipartite_graph.order
     matching = BipartiteGraphMatching(order)
     
-    # Пока паросочетание не совершенное
-    while matching.cardinality < order:        
+    while not matching.is_perfect:        
         start_node = -1
         for left_node in range(order):
             if not matching.is_left_covered(left_node):
                 start_node = left_node
-                break
-        
+                break   
 
-        queue = deque([start_node])
-        
-        #Хранит левую вершину из которой пришли в правую
-        parents: dict[int, int] = {}        
-        path_end = -1
-
-        while queue:
-            left_node = queue.popleft() 
-            neighbors = bipartite_graph.right_neighbors(left_node)
-            
-            for right_node in neighbors:
-                if right_node in parents:
-                    continue
-                parents[right_node] = left_node
-                
-                if not matching.is_right_covered(right_node):
-                    path_end = right_node
-                    break
-                else:
-                    next_left = matching.get_left_match(right_node)
-                    queue.append(next_left)
-            if path_end != -1:
-                break
-        
-        
+        result = find_end_node(start_node,matching,bipartite_graph)
+        path_end = result[0]
+        parents = result[1]
         if path_end != -1:
-            curr_right = path_end
-
-            #Восстановление пути
-            while curr_right is not None:
-                parent_left = parents[curr_right]
-
-                next_right_in_path = None
-
-                if matching.is_left_covered(parent_left):                    
-                    next_right_in_path = matching.get_right_match(parent_left)
-                    matching.remove_edge(parent_left, next_right_in_path)
-                
-                matching.add_edge(parent_left, curr_right)
-                curr_right = next_right_in_path
-                
+            repaint(path_end, parents,matching, bipartite_graph)
         else:          
             raise PerfectMatchingError(ErrorMessageEnum.NOT_EXISTED_PERFECT_MATCH)
 
     return matching
+
+def repaint(path_end, parents, matching, bipartite_graph):
+    """Принимает последнюю точку пути а также словарь пути и перекрашивает цепь"""
+    curr_right = path_end
+     #Восстановление пути
+    while curr_right is not None:
+        parent_left = parents[curr_right]
+        next_right_in_path = None
+
+        if matching.is_left_covered(parent_left):                    
+            next_right_in_path = matching.get_right_match(parent_left)
+            matching.remove_edge(parent_left, next_right_in_path)
+        
+        matching.add_edge(parent_left, curr_right)
+        curr_right = next_right_in_path   
+
+def find_end_node(start_node : int, matching,bipartite_graph):
+    """Находит последний узел а также путь цепочки"""
+    queue = deque([start_node])
+    #Хранит левую вершину из которой пришли в правую 
+    parents: dict[int, int] = {}        
+    path_end = -1   
+    
+    while queue:
+            left_node = queue.popleft() 
+            neighbors = bipartite_graph.right_neighbors(left_node)            
+            for right_node in neighbors:
+                if right_node in parents:
+                    continue
+                parents[right_node] = left_node                
+                if not matching.is_right_covered(right_node):
+                    path_end = right_node
+                    return [path_end, parents]
+                else:
+                    next_left = matching.get_left_match(right_node)
+                    queue.append(next_left)
+    return [path_end, parents]
+        
+
+
 
 if __name__ == "__main__":
     print("Исходный двудольный граф")
