0a4e4bb1c6d23d0a04c4e7be07c688e9408ab0fe
Poruch <139491864+Poruch@users.noreply.github.com>
2026-02-17 21:45:09 +0500

    complete task

--- network_flow/max_flow_calculator.py
+++ network_flow/max_flow_calculator.py
@@ -25,18 +25,14 @@ class MaxFlowCalculator:
         self._order = len(capacity_matrix)
         self._source_idx = vertices.sources[0]
         self._sink_idx = vertices.sinks[0]
-
-        self._residual_matrix = [[0] * self._order for _ in range(self._order)]
+        
         for row_idx in range(self._order):
             for col_idx in range(self._order):
                 if capacity_matrix[row_idx][col_idx]:
-                    self._residual_matrix[col_idx][row_idx] = self._capacity_matrix[
-                        row_idx
-                    ][col_idx]
+                    self._residual_matrix[col_idx][row_idx] = self._capacity_matrix[row_idx][col_idx]
 
         self._max_flow = None
         self._flow_matrix = None
-        # Процедура _calculate_max_flow должна в ходе выполнения заполнить атрибуты _flow_matrix и _max_flow
         self._calculate_max_flow()
 
     @property
@@ -53,50 +49,108 @@ class MaxFlowCalculator:
     def split_vertices_by_types(matrix) -> NetworkVerticesData:
         """
         Разделяет вершины сети на три категории: источники, стоки и транзитные вершины.
-
         :param matrix: Квадратная матрица пропускных способностей графа.
         :type matrix: list[list[int]]
         :return: Данные о вершинах сети, содержащие источники, стоки и транзиты.
         :rtype: NetworkVerticesData
         """
+        n = len(matrix)
+        row_sum = [sum(row) for row in matrix]
+        col_sum = [sum(matrix[i][j] for i in range(n)) for j in range(n)]  
+
         sources = []
-        ...
-
         sinks = []
-        ...
-
         transits = []
-        ...
+
+        for i in range(n):
+            has_outgoing = row_sum[i] > 0
+            has_incoming = col_sum[i] > 0
+
+            if not has_incoming:
+                sources.append(i) 
+            elif not has_outgoing and has_incoming:
+                sinks.append(i)  
+            else:
+                transits.append(i)                     
 
         return NetworkVerticesData(sources, sinks, transits)
 
     def _calculate_max_flow(self) -> None:
-        """Вычисляет максимальный поток в сети с использованием алгоритма Форда-Фалкерсона"""
-        ...
+        """Вычисляет максимальный поток алгоритмом Форда-Фалкерсона"""
+        self._flow_matrix = [[0] * self._order for _ in range(self._order)]
+        self._max_flow = 0        
+        
+        while True:
+            path = self._find_augmenting_path()
+            if not path:
+                break
+
+            path_flow = inf
+            for i in range(len(path) - 1):
+                first_vertex = path[i]
+                second_vertex = path[i + 1]
+                cap = self._residual_matrix[second_vertex][first_vertex]
+                if cap < path_flow:
+                    path_flow = cap
+
+            self._increase_flow(path, path_flow)
+            self._max_flow += path_flow
+
+        self._set_flow_matrix_by_residual_matrix()
 
     def _set_flow_matrix_by_residual_matrix(self):
-        """Обновляет матрицу локальных потоков на основе остаточной сети"""
-        ...
+        """Заполняет матрицу потоков на основе остаточной сети"""
+        for i in range(self._order):
+            for j in range(self._order):
+                if self._capacity_matrix[i][j] > 0:
+                    self._flow_matrix[i][j] = self._residual_matrix[i][j]
 
     def _find_augmenting_path(self):
-        """Возвращает найденный увеличивающий путь в сети"""
-        ...
+        """
+        Возвращает увеличивающий путь в виде списка вершин от истока к стоку.
+        """
+        visited = [False] * self._order
+        parent = [-1] * self._order
+        queue = deque([self._sink_idx])
+        visited[self._sink_idx] = True
 
-    def _increase_flow(self, augmenting_path):
-        """Корректирует остаточную сеть для увеличения потока в сети с использованием
-        найденного увеличивающего пути"""
-        ...
+        while queue:
+            start_vertex = queue.popleft()
+            for vertex in range(self._order):
+                if not visited[vertex] and self._residual_matrix[start_vertex][vertex] > 0:
+                    visited[vertex] = True
+                    parent[vertex] = start_vertex
+                    if vertex == self._source_idx:
+                        path = []
+                        cur = vertex
+                        while cur != -1:
+                            path.append(cur)
+                            cur = parent[cur]
+                        return path 
+                    queue.append(vertex)
+        return None
+
+    def _increase_flow(self, path, path_flow: int):
+        """
+        Корректирует остаточную сеть: уменьшает пропускную способность обратных рёбер
+        и увеличивает прямые.
+        """
+        for i in range(len(path) - 1):
+            first_vertex = path[i]
+            second_vertex = path[i + 1]
+            self._residual_matrix[second_vertex][first_vertex] -= path_flow
+            self._residual_matrix[first_vertex][second_vertex] += path_flow
 
 
 if __name__ == "__main__":
     vertex_names = ["s", "a", "b", "c", "d", "t"]
     capacity_matrix = [
-        # s a  b  c  d  t
         [0, 7, 7, 7, 0, 0],  # s
         [0, 0, 0, 6, 9, 0],  # a
         [0, 6, 0, 5, 0, 0],  # b
-        [0, 0, 0, 0, 11, 0],  # c
-        [0, 0, 0, 0, 0, 13],  # d
+        [0, 0, 0, 0, 11, 0], # c
+        [0, 0, 0, 0, 0, 13], # d
+        [0, 0, 0, 0, 0, 13], # d
         [0, 0, 0, 0, 0, 0],  # t
     ]
     print("Матрица пропускной способности")
